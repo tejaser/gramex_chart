7e6a56b73a3629bdb7a3cb291045de6622412fda
Tejas Pancholi <tejas.er@outlook.com>
2018-03-28 23:25:50 +0530

    data parsing completed.

--- js/common.js
+++ js/common.js
@@ -2,7 +2,18 @@ $.ajaxSetup({
   async:false
 });
 
-d3.json('/product_quantity', function(data){
+d3.json('/product_quantity', function(error, data){
   $('#product_quantity_chart').empty();
   console.log(data)
+  data.forEach(function(d) {
+    d.QUANTITY = +d.QUANTITY;
+    d.TOT_PRICE = +d.TOT_PRICE;
+    d.PRICE = +d.PRICE;
+  })
+
+  var xMax = d3.max(data,  function(d) {return d.QUANTITY})
+  var xExtent = d3.extent(data, d => d.QUANTITY)
+  console.log(xMax, xExtent)
+
+  // console.log(data)
 })
--- product_quantity.py
+++ product_quantity.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import json
 
-def read_data():
+def read_data(handler):
   """reading pandas data"""
   data = pd.read_csv('./data/Products_1320.csv')
   df = data[data['CLICK_SOURCE'] != 'Direct']
-  result = df.to_json(orient=records)
+  result = df.to_json(path_or_buf = None, orient = 'records', date_format = 'epoch', double_precision = 10, force_ascii = True, date_unit = 'ms', default_handler = None)
   return result
