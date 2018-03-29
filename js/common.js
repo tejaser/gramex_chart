2782ac0922fb0a418740402f1f36246ff6ddb0d7
tejaser <tejas.er@outlook.com>
2018-03-29 17:44:03 +0530

    added scatter plot

--- js/common.js
+++ js/common.js
@@ -1,19 +1,78 @@
 $.ajaxSetup({
-  async:false
+  async: false
 });
 
-d3.json('/product_quantity', function(error, data){
+d3.json('/product_quantity', function (error, data) {
   $('#product_quantity_chart').empty();
-  console.log(data)
-  data.forEach(function(d) {
+  // console.log(data)
+
+  data.forEach(function (d) {
     d.QUANTITY = +d.QUANTITY;
     d.TOT_PRICE = +d.TOT_PRICE;
     d.PRICE = +d.PRICE;
   })
+  const width = 960;
+  const height = 500;
+  const xValue = d => d.QUANTITY;
+  const xLabel = 'Quantity of Product';
+  const yValue = d => d.TOT_PRICE;
+  const yLabel = 'Total Price';
+  const margin = {left: 150, right: 70, top: 20, bottom: 110};
+  const innerHeight = height - margin.top - margin.bottom;
+  const svg = d3.select("#product_quantity_chart").append("svg:svg")
+    .attr("width", width)
+    .attr("height", height);
+  const g = svg.append('g')
+    .attr('transform', `translate(${margin.left},${margin.top})`);
+  const xAxisG = g.append('g')
+    .attr('transform', `translate(0, ${innerHeight})`);
+  const yAxisG = g.append('g');
+  const innerWidth = width - margin.left - margin.right;
 
-  var xMax = d3.max(data,  function(d) {return d.QUANTITY})
-  var xExtent = d3.extent(data, d => d.QUANTITY)
-  console.log(xMax, xExtent)
+  xAxisG.append('text')
+    .attr('class', 'axis-label')
+    .attr('x', innerWidth / 2)
+    .attr('y', 90)
+    .text(xLabel);
 
-  // console.log(data)
+  yAxisG.append('text')
+    .attr('class', 'axis-label')
+    .attr('x', -innerHeight / 2)
+    .attr('y', -100)
+    .attr('transform', `rotate(-90)`)
+    .style('text-anchor', 'middle')
+    .text(yLabel);
+
+  const xScale = d3.scalePoint();
+  const yScale = d3.scaleLinear();
+  const xAxis = d3.axisBottom()
+    .scale(xScale)
+    .tickPadding(15)
+    .tickSize(-innerHeight);
+
+  const yTicks = 5;
+  const yAxis = d3.axisLeft()
+    .scale(yScale)
+    .ticks(yTicks)
+    .tickPadding(15)
+    .tickFormat(d3.format('.0s'))
+    .tickSize(-innerWidth);
+  xScale
+    .domain(data.map(xValue))
+    .range([0, innerWidth]);
+
+  yScale
+    .domain(d3.extent(data, yValue))
+    .range([innerHeight, 0])
+    .nice(yTicks);
+  g.selectAll('circle').data(data)
+    .enter().append('circle')
+    .attr('cx', d => xScale(xValue(d)))
+    .attr('cy', d => yScale(yValue(d)))
+    .attr('fill', 'black')
+    .attr('fill-opacity', 0.6)
+    .attr('r', 8);
+
+  xAxisG.call(xAxis);
+  yAxisG.call(yAxis);
 })
--- product_quantity.py
+++ product_quantity.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import json
 
+
 def read_data(handler):
-  """reading pandas data"""
-  data = pd.read_csv('./data/Products_1320.csv')
-  df = data[data['CLICK_SOURCE'] != 'Direct']
-  result = df.to_json(path_or_buf = None, orient = 'records', date_format = 'epoch', double_precision = 10, force_ascii = True, date_unit = 'ms', default_handler = None)
-  return result
+    """reading pandas data"""
+    data = pd.read_csv('./data/Products_1320.csv')
+    print('inside the function')
+    df = data[data['CLICK_SOURCE'] != 'Direct']
+    result = df.to_json(path_or_buf=None, orient='records', date_format='epoch', double_precision=10, force_ascii=True,
+                        date_unit='ms', default_handler=None)
+    return result
